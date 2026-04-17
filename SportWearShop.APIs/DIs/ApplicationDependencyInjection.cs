08b2e28fbb4c5e6dfd4f10bb376f707331ab19ca
Huy Nguyễn <huynvtse171283@fpt.edu.vn>
2026-04-17 18:33:10 +0700

    Repository Setup and Try to fetch database

---
+++ SportWearShop.APIs/DIs/ApplicationDependencyInjection.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using SportWearShop.BusinessLogics;
+using SportWearShop.Repositories; // ?
+using SportWearShop.Repositories.Entities; // ?
+
+namespace SportWearShop.APIs.DIs
+{
+    public static class ApplicationDependencyInjection
+    {
+        public static IServiceCollection AddDependencyInjection(
+        this IServiceCollection services,
+        IConfiguration configuration)
+        {
+            // Register DbContext
+            services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(
+                    configuration.GetConnectionString("DefaultConnection"),
+                    b => b.MigrationsAssembly("SportWearShop.Repositories")));
+
+            services.AddBusinessLayer(configuration);
+
+            // ???? 
+            services.AddIdentity<AppUser, AppRole>()
+                .AddEntityFrameworkStores<AppDbContext>()
+                .AddDefaultTokenProviders();
+
+            
+
+
+            return services;
+        }
+    }
+}
