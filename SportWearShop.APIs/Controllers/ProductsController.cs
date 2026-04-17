32f91e2c75d68cd33f38ff621873fb512359ca8c
Huy Nguyễn <huynvtse171283@fpt.edu.vn>
2026-04-17 18:33:10 +0700

    Repository Setup and Try to fetch database

---
+++ SportWearShop.APIs/Controllers/ProductsController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SportWearShop.Repositories;
+using SportWearShop.Repositories.Entities;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SportWearShop.APIs.Controllers
+{
+    [Route("api/product")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        public ProductsController(AppDbContext context)
+        {
+            _context = context;
+        }
+        // GET: api/product
+        [HttpGet]
+        public async Task<List<Product>> GetAllAsync()
+        {
+            var ps = await _context.Products.ToListAsync();
+            return ps;
+        }
+
+        // GET api/product/5
+        [HttpGet("{id}")]
+        public async Task<Product> GetAsync(int id)
+        {
+            var p = await _context.Products.FindAsync(id);
+            return p == null ? null : p;
+        }
+
+        
+    }
+}
--- SportWearShop.APIs/Program.cs
+++ SportWearShop.APIs/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi;
+using SportWearShop.APIs.DIs;
 using SportWearShop.Repositories;
 using SportWearShop.Repositories.Entities;
 
@@ -7,28 +9,38 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
-
-// Registry Database Context
-builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(
-        builder.Configuration.GetConnectionString("DefaultConnection"),
-        b => b.MigrationsAssembly("SportWearShop.Repositories") // instead of --dir-output
-    ));
-
-// Registry Identity services
+// register dependencies from business layer
+builder.Services.AddDependencyInjection(builder.Configuration);
+/*
 builder.Services.AddIdentity<AppUser, AppRole>()
-    .AddEntityFrameworkStores<AppDbContext>()
-    .AddDefaultTokenProviders();
+        .AddEntityFrameworkStores<AppDbContext>()
+        .AddDefaultTokenProviders();*/
+
+builder.Services.AddControllers();
+
+// Swagger / OpenAPI
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "SportWearShop API",
+        Version = "v1",
+        Description = "API for SportWearShop project"
+    });
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "SportWearShop API v1");
+        options.RoutePrefix = "swagger";
+    });
 }
 
 app.UseHttpsRedirection();
